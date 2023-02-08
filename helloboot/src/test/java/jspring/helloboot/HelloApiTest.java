7f89d8be14048125bbfe4816b40be0872ba9dce0
baejinsoo <jinsoo941010@naver.com>
2023-02-08 15:04:51 +0900

    테스트 코드 추가

---
+++ helloboot/src/test/java/jspring/helloboot/HelloApiTest.java
@@ -0,0 +1,29 @@
+package jspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        // ResponseEntity : web 응답의 모든 요소를 가지고 있는 object
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        /* 응답 검증 */
+        // status code 200
+        Assertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        Assertions.assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        Assertions.assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}
