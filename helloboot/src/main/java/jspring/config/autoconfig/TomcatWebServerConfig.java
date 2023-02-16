6f879bdca47d2d05e578b20b49cb552a15d5f1f6
baejinsoo <jinsoo941010@naver.com>
2023-02-16 17:38:27 +0900

    @Conditinal 커스텀하기: MyOnClassCondition 생성

--- helloboot/src/main/java/jspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/jspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,27 +1,20 @@
 package jspring.config.autoconfig;
 
+import jspring.config.ConditionalMyOnClass;
 import jspring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
+/**
+ * 어떤걸 기준으로 tomcat으로 띄울지 jetty로 띄울지 결정하면 좋을까?
+ * => 어떤 라이브러리가 이 프로젝트에 포함되어 잇는가?
+ */
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
-    }
-
-    static class TomcatCondition implements Condition {
-
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
-        }
     }
 }
